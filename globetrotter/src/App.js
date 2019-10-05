ed15e44e279e6b3aa58b0f93a43fcdc244765eff
Akinnusoye Taiwo <taiwoakinnusoye@gmail.com>
2019-10-05 18:50:13 +0100

    boostrapped application, checked plumbing

--- globetrotter/src/App.js
+++ globetrotter/src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from 'react';
+import {useQuery} from '@apollo/react-hooks';
+import {gql} from 'apollo-boost';
+
+const ALL_COUNTRIES_QUERY = gql`
+  {
+    countries {
+      name
+      code
+    }
+  }
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const {data, error, loading} = useQuery(ALL_COUNTRIES_QUERY);
+   if (loading) console.log('data is loading');
+   if (error) console.log('error when getting data');
+
+   return (
+    <Fragment>
+      {data &&
+        data.countries.map(({name, code}) => (
+          <p key={code}>
+            {name}
+          </p>
+        ))}
+    </Fragment>
   );
 }
 
--- globetrotter/src/index.js
+++ globetrotter/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from '@apollo/react-hooks';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const client = new ApolloClient({
+  uri: 'https://countries.trevorblades.com'
+});
+
+ReactDOM.render(<ApolloProvider client={client}><App /></ApolloProvider>, document.getElementById('root'));
+
+
+
