dfd50992c8042f34609e8286cf19b0eab4a0acf9
Akinnusoye Taiwo <taiwoakinnusoye@gmail.com>
2019-10-10 17:24:48 +0100

    country container

--- globetrotter/src/components/CountriesComponent.js
+++ globetrotter/src/components/CountriesComponent.js
@@ -1,15 +1,26 @@
 import React, {Fragment} from 'react';
-// import NavComponent from './NavComponent';
-// import LoadingComponent from './LoadingComponent';
 
 
 function CountriesComponent ({countries}) {
 
    return (
     <Fragment>
-        <p>
-            {countries.name}
-        </p>
+        <div className="country-card">
+            <div className="country-code">
+            <p>{countries.code}, </p>
+            </div>
+            <div className="country-name">
+            <p>{countries.name}, </p>
+            </div>
+            <div className="country-languages">
+            {countries.languages.map(language => (
+                <span key={language.name}>{language.native}, </span>
+            ))}
+            </div>
+            <div className="country-continent">
+            <p>{countries.continent.name}</p>
+            </div>
+        </div>
     </Fragment>
   );
 }
--- globetrotter/src/containers/Countries.js
+++ globetrotter/src/containers/Countries.js
@@ -5,6 +5,17 @@ import CountriesComponent from '../components/CountriesComponent';
 import NavComponent from '../components/NavComponent';
 import LoadingComponent from '../components/LoadingComponent';
 
+import styled from 'styled-components';
+
+const CountriesGrid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(6, 200px);
+    grid-column-gap: 15px;
+    justify-content: center;
+    background: #130377;
+`;
+
+
 const ALL_COUNTRIES_QUERY = gql`
   {
     countries {
@@ -31,10 +42,12 @@ function Countries () {
  return (
   <Fragment>
       <NavComponent />
+      <CountriesGrid>
     {data &&
       data.countries.map(country => (
         <CountriesComponent key={country.code} countries={country} />
       ))}
+      </CountriesGrid>
   </Fragment>
 );
 }
